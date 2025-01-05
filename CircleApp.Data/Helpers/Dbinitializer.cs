f2dc8a85154d67692bb577fb547578699cd2eeea
Sagar Bhandari <sagarvandari1@gmail.com>
2025-01-05 13:11:39 +1300

    feat:Read and display post from database

--- CircleApp.Data/Helpers/Dbinitializer.cs
+++ CircleApp.Data/Helpers/Dbinitializer.cs
@@ -19,7 +19,9 @@ namespace CircleApp.Data.Helpers
             {
                 var User = new User()
                 {
-                    FullName = "Sagar Bhandari"
+                    FullName = "Sagar Bhandari",
+                    ProfilePictureUrl= "https://images.unsplash.com/photo-1542613172-4ac1d7351721?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
 
                 };
                 await context.Users.AddAsync(User);
--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using CircleApp.Data;
 using CircleApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CircleApp.Controllers
@@ -7,15 +9,20 @@ namespace CircleApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public  IActionResult Index()
         {
-            return View();
+            var allPosts = _context.Posts
+                .Include(n => n.User)
+                .ToList();
+            return View(allPosts);
         }
 
         public IActionResult Privacy()
