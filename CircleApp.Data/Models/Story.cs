2e4cf8e12f35407763c9155dc9a694120864de9e
Sagar Bhandari <sagarvandari1@gmail.com>
2025-02-24 13:37:43 +1300

    feat: create stroy model

---
+++ CircleApp.Data/Models/Story.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp.Data.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool IsDeleted { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+    }
+}
