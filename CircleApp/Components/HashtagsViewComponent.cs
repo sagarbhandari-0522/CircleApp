6ba5df88836d0ffbd9ec686f06cd812afd47510b
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-03 23:05:21 +1300

    feat: hashtag view component

---
+++ CircleApp/Components/HashtagsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Components
+{
+    public class HashtagsViewComponent :ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public HashtagsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
