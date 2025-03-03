5f6e186efe6832e77cf3bf6850bc6bef305a3459
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-03 23:04:25 +1300

    feat: simple console application for debugging

---
+++ CircleApp.Data/Models/Hashtag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp.Data.Models
+{
+    public class Hashtag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Count { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CircleApp.Data.Helpers;
+class Program
+{
+    public static void Main()
+    {
+        HashtagHelper.Hashtag(3,"Hello #word! #how! !are #you #word");
+    }
+}
