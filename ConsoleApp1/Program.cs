e25ce8ba20d7856286fa54a662d35f4458b0b46c
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-05 22:46:18 +1300

    Stop tracking ConsoleApp

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CircleApp.Data.Helpers;
-class Program
-{
-    public static void Main()
-    {
-        HashtagHelper.Hashtag(3,"Hello #word! #how! !are #you #word");
-    }
-}
