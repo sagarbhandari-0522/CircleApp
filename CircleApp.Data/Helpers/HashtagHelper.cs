547b8c2d5df3650e39670f79b7d8d0a1c66b4443
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-05 22:48:45 +1300

    feat: hashtag service

--- CircleApp.Data/Helpers/HashtagHelper.cs
+++ CircleApp.Data/Helpers/HashtagHelper.cs
@@ -10,40 +10,8 @@ namespace CircleApp.Data.Helpers
 {
     public class HashtagHelper
     {
-        public static ApplicationDbContext _context;
-        public static void Hashtag( ApplicationDbContext context, string postContent)
+        public static List<string> GetHashTag( string postContent)
         {
-           _context = context;
-            var saveTagsName = _context.Hashtags.Select(n => n.Name).ToList();
-            var matches = Regex.Matches(postContent, @"#\w+");
-
-
-            var hashtags=matches.Select(m=>m.Value.Replace(@"[^a-z0-9#]","").ToLower()).Distinct().ToList();
-            hashtags.ForEach(tag =>
-            {   
-                if(saveTagsName.Contains(tag))
-                {
-                    var hashTag = _context.Hashtags.Where(n => n.Name == tag).First();
-                    hashTag.Count += 1;
-                    hashTag.UpdatedAt = DateTime.UtcNow;
-                    _context.Hashtags.Update(hashTag);
-                }
-                else
-                {
-                    Hashtag hashTag = new Hashtag();
-                    hashTag.Name = tag;
-                    hashTag.CreatedAt = DateTime.UtcNow;
-                    hashTag.UpdatedAt = DateTime.UtcNow;
-                    hashTag.Count = 1;
-                    _context.Hashtags.Add(hashTag);
-                    _context.SaveChanges();
-                }
-            });
-        }
-        public static List<string> GetHashTag(ApplicationDbContext context, string postContent)
-        {
-            _context = context;
-            var saveTagsName = _context.Hashtags.Select(n => n.Name).ToList();
             var matches = Regex.Matches(postContent, @"#\w+");
 
 
@@ -51,5 +19,6 @@ namespace CircleApp.Data.Helpers
 
             return hashtags;
         }
+       
     }
 }
