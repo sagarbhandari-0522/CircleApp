e09fb70458e6fea4b9af98d90e579a3e76ccdbd7
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-06 22:27:52 +1300

    feat: service class for uploading file

---
+++ CircleApp/Services/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using CircleApp.Data.Helpers;
+
+namespace CircleApp.Services
+{
+    public interface IFileService
+    {
+        public string UploadFile(IFormFile image, ImageType imageType);
+    }
+}
