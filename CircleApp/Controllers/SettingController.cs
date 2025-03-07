72fba163da9ab00fb30816ef274522ae514012a5
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-07 14:09:51 +1300

    feat: setting controller with index action and view

---
+++ CircleApp/Controllers/SettingController.cs
@@ -0,0 +1,21 @@
+﻿using CircleApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class SettingController : Controller
+    {
+        private readonly IUserSettingService _userSetting;
+        public SettingController(IUserSettingService userSetting)
+        {
+            _userSetting=userSetting;
+        }
+        
+        public IActionResult Index()
+        {
+            var currentUserId = 1;
+            var user = _userSetting.GetUserDetails(currentUserId);
+            return View(user);
+        }
+    }
+}
