4147822a197808a0b1c0f929f1108f72eb91d1c8
Sagar Bhandari <sagarvandari1@gmail.com>
2025-03-20 16:20:04 +1300

    feat: Login User

---
+++ CircleApp/ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CircleApp.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
