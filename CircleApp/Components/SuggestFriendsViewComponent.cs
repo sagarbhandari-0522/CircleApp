d8cf06bf50d0d9676b25585d2d6b6e03eb33c93d
Sagar Bhandari <sagarvandari1@gmail.com>
2025-04-10 13:16:10 +1200

    feat: create suggestfriend component

---
+++ CircleApp/Components/SuggestFriendsViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Components
+{
+    public class SuggestFriendsViewComponent :ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
